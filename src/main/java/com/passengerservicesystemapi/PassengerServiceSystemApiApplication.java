b8d48a3a846f7e2b0165795afb884d0d004ef9e3
Josafat Zavaleta <53987304+JosafatZG@users.noreply.github.com>
2023-02-09 23:21:53 -0600

    Initial commit

---
+++ src/main/java/com/passengerservicesystemapi/PassengerServiceSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.passengerservicesystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerServiceSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerServiceSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/passengerservicesystemapi/PassengerServiceSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passengerservicesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerServiceSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
